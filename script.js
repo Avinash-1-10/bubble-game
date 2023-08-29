d4ef52947055b3a65e78df28f44e0c5e53c0d793
Avinash-1-10 <avinashyede110@gmail.com>
2023-08-29 21:40:50 +0530

    project complete

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-function makeBubble(number){
-    let pbtm = document.querySelector("#pbtm")
-    let clutter = ""
-    for(let i =0; i<=number; i++){
-        clutter += `<div class="bubble">${Math.floor(Math.random()*10)+1 }</div>`
-      }
-      pbtm.innerHTML= clutter; 
+let timer = 60;
+let score = 0;
+
+function scoreIncrement() {
+  score += 10;
+  document.querySelector("#score").textContent = score;
 }
 
-makeBubble(200)
+function generateHit() {
+  let hit = Math.floor(Math.random() * 10);
+  document.querySelector("#hit").textContent = hit;
+}
 
-let time = 10;
+function makeBubble(number) {
+  let pbtm = document.querySelector("#pbtm");
+  let clutter = "";
+  for (let i = 0; i <= number; i++) {
+    clutter += `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`;
+  }
+  pbtm.innerHTML = clutter;
+}
 
-const interval = setInterval(() => {
-    time -= 1;
-    if (time <= 0 || time >= 60) {
-        clearInterval(interval);
+function runTimer() {
+  let timeCounter = setInterval(() => {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timer").textContent = timer;
+    } else {
+      document.querySelector(
+        "#pbtm"
+      ).innerHTML = `<h1 class="scorecard">Your Score is ${score}</h1>`;
+      clearInterval(timeCounter);
     }
-    console.log(time);
-}, 1000);
+  }, 1000);
+}
 
+document.querySelector("#pbtm").addEventListener("click", (details) => {
+  const value = +details.target.textContent;
+  const targetValue = +document.querySelector("#hit").textContent;
+  if (value === targetValue) {
+    scoreIncrement();
+    makeBubble(200);
+    generateHit();
+  }
+});
 
+makeBubble(200);
+runTimer();
+generateHit();
